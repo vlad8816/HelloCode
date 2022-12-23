be63d9e3501f8bd1d537f6472988d9785cf85a11
Vlad <email@gmail.com>
2022-12-23 11:39:53 +0500

    Example010 done

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;// алгоритм завершил работу, так как уже нашел индекс искомого числа, без break, он бы показал 4 и 8, то есть все индексы искомого числа в массиве 
+    }
+    //index= index+1;
+    index++;
+}
